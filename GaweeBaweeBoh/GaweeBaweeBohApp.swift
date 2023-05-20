1551ae2b4bf379b75a5173747def796cb632c7eb
서창열 <kongbaguni@gmail.com>
2023-05-20 21:41:26 +0900

    Initial Commit

---
+++ GaweeBaweeBoh/GaweeBaweeBohApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaweeBaweeBohApp.swift
+//  GaweeBaweeBoh
+//
+//  Created by Changyeol Seo on 2023/05/20.
+//
+
+import SwiftUI
+
+@main
+struct GaweeBaweeBohApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
